e6006ba3c63c52daa41c4e33b1e93041c21ff8d8
Tim van Elsloo <tim@glacyr.com>
2022-08-10 20:11:05 +0200

    Moved output types to a different file.

--- kono-aspect/src/lib.rs
+++ kono-aspect/src/lib.rs
@@ -1,17 +1,17 @@
 mod aspect;
 mod error;
 mod inputs;
+mod outputs;
 mod record;
 mod reference;
 mod resolver;
-mod types;
 mod value;
 
 pub use aspect::Aspect;
 pub use error::Error;
 pub use inputs::InputType;
+pub use outputs::OutputType;
 pub use record::{Record, RecordResolver};
 pub use reference::Reference;
 pub use resolver::{AspectExt, AspectResolver};
-pub use types::OutputType;
 pub use value::ObjectValue;
