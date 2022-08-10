cf5c0f8879fb41d0b75d245af15e2cf3bbc249fc
Tim van Elsloo <tim@glacyr.com>
2022-08-10 19:15:27 +0200

    Improved implementation of input types.

---
+++ kono-aspect/src/inputs.rs
@@ -0,0 +1,169 @@
+use std::convert::{TryFrom, TryInto};
+
+use kono_executor::{Error, Value};
+use kono_schema::{Item, ItemScalar, Type};
+
+pub trait InputType<Env> {
+    fn ty(environment: &Env) -> Type;
+
+    fn schema(environment: &Env) -> Vec<Item>;
+
+    fn from_value<E>(value: Value) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error;
+
+    fn from_value_option<E>(value: Option<Value>) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error,
+    {
+        match value {
+            Some(value) => Self::from_value(value),
+            None => Err(E::missing_value()),
+        }
+    }
+}
+
+macro_rules! ty {
+    ($ty:ty, $name:literal, $($from:tt)*) => {
+        impl<Env> InputType<Env> for $ty {
+            fn ty(_environment: &Env) -> Type {
+                Type::Scalar($name.to_owned())
+            }
+
+            fn schema(_environment: &Env) -> Vec<Item> {
+                vec![Item::Scalar(ItemScalar::new($name))]
+            }
+
+            fn from_value<E>(value: Value) -> Result<Self, E>
+            where
+                Self: Sized,
+                E: Error,
+            {
+                match value {
+                    $($from)*,
+                    _ => Err(E::missing_value()),
+                }
+            }
+        }
+    };
+}
+
+trait AsNumber<T> {
+    fn as_number(self) -> Option<T>;
+}
+
+impl AsNumber<u64> for serde_json::Number {
+    fn as_number(self) -> Option<u64> {
+        self.as_u64()
+    }
+}
+
+impl AsNumber<i64> for serde_json::Number {
+    fn as_number(self) -> Option<i64> {
+        self.as_i64()
+    }
+}
+
+impl AsNumber<f64> for serde_json::Number {
+    fn as_number(self) -> Option<f64> {
+        self.as_f64()
+    }
+}
+
+trait FromNumber<T>: TryFrom<T>
+where
+    serde_json::Number: AsNumber<T>,
+{
+    fn from_number<E>(value: serde_json::Number) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error,
+    {
+        value
+            .as_number()
+            .ok_or(E::custom("can't represent number"))?
+            .try_into()
+            .map_err(|_| E::custom("can't represent number"))
+    }
+}
+
+impl<T, U> FromNumber<U> for T
+where
+    T: TryFrom<U>,
+    serde_json::Number: AsNumber<U>,
+{
+}
+
+ty!(String, "String", Value::String(value) => Ok(value));
+ty!(bool, "Boolean", Value::Bool(value) => Ok(value));
+ty!(u8, "Int", Value::Number(value) => FromNumber::<u64>::from_number(value));
+ty!(u16, "Int", Value::Number(value) => FromNumber::<u64>::from_number(value));
+ty!(u32, "Int", Value::Number(value) => FromNumber::<u64>::from_number(value));
+ty!(u64, "Int", Value::Number(value) => FromNumber::<u64>::from_number(value));
+ty!(i8, "Int", Value::Number(value) => FromNumber::<i64>::from_number(value));
+ty!(i16, "Int", Value::Number(value) => FromNumber::<i64>::from_number(value));
+ty!(i32, "Int", Value::Number(value) => FromNumber::<i64>::from_number(value));
+ty!(i64, "Int", Value::Number(value) => FromNumber::<i64>::from_number(value));
+ty!(f32, "Float", Value::Number(value) => FromNumber::<f64>::from_number(value).map(|value: f64| value as f32));
+ty!(f64, "Float", Value::Number(value) => FromNumber::<f64>::from_number(value));
+
+impl<Env, T> InputType<Env> for Option<T>
+where
+    T: InputType<Env>,
+{
+    fn ty(environment: &Env) -> Type {
+        T::ty(environment)
+    }
+
+    fn schema(environment: &Env) -> Vec<Item> {
+        T::schema(environment)
+    }
+
+    fn from_value<E>(value: Value) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error,
+    {
+        match value {
+            Value::Null => Ok(None),
+            value => T::from_value(value).map(Some),
+        }
+    }
+
+    fn from_value_option<E>(value: Option<Value>) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error,
+    {
+        match value {
+            Some(value) => Self::from_value(value),
+            None => Ok(None),
+        }
+    }
+}
+
+impl<Env, T> InputType<Env> for Vec<T>
+where
+    T: InputType<Env>,
+{
+    fn ty(environment: &Env) -> Type {
+        T::ty(environment)
+    }
+
+    fn schema(environment: &Env) -> Vec<Item> {
+        T::schema(environment)
+    }
+
+    fn from_value<E>(value: Value) -> Result<Self, E>
+    where
+        Self: Sized,
+        E: Error,
+    {
+        match value {
+            Value::Array(items) => items.into_iter().map(InputType::from_value).collect(),
+            _ => Err(E::custom("expected array")),
+        }
+    }
+}
--- kono-aspect/src/lib.rs
+++ kono-aspect/src/lib.rs
@@ -1,5 +1,6 @@
 mod aspect;
 mod error;
+mod inputs;
 mod record;
 mod reference;
 mod resolver;
@@ -9,9 +10,10 @@ mod value;
 
 pub use aspect::Aspect;
 pub use error::Error;
+pub use inputs::InputType;
 pub use record::{Record, RecordResolver};
 pub use reference::Reference;
 pub use resolver::{AspectExt, AspectResolver};
 pub use traits::IntoIntermediate;
-pub use types::{InputType, OutputType};
+pub use types::OutputType;
 pub use value::ObjectValue;
