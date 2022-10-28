fdcc6f20047d09925cc419ee2b945bf55ed64b89
Tim van Elsloo <tim@glacyr.com>
2022-10-28 12:07:13 +0200

    Restructured `litho-validation`.

--- litho-validation/src/common/mod.rs
+++ litho-validation/src/common/mod.rs
@@ -6,15 +6,16 @@ use litho_language::ast::*;
 use litho_language::lex::Span;
 use litho_types::Database;
 
-mod common;
-mod system;
+mod values;
 
 pub fn check<N, T>(document: &N, database: &Database<T>) -> Vec<Diagnostic<Span>>
 where
     N: Node<T>,
     T: Eq + Hash + Borrow<str> + ToString,
 {
-    let mut errors = system::check(document, database);
-    errors.extend(common::check(document, database));
+    let mut errors = vec![];
+    document.traverse(&values::EnumCoercion(database), &mut errors);
+    document.traverse(&values::InputCoercion(database), &mut errors);
+    document.traverse(&values::ObjectCoercion(database), &mut errors);
     errors
 }
---
+++ litho-validation/src/system/mod.rs
@@ -0,0 +1,44 @@
+use std::borrow::Borrow;
+use std::hash::Hash;
+
+use litho_diagnostics::Diagnostic;
+use litho_language::ast::*;
+use litho_language::lex::Span;
+use litho_types::Database;
+
+mod arguments;
+mod directives;
+mod enums;
+mod extensions;
+mod fields;
+mod inputs;
+mod interfaces;
+mod names;
+mod types;
+mod unions;
+
+pub fn check<N, T>(document: &N, database: &Database<T>) -> Vec<Diagnostic<Span>>
+where
+    N: Node<T>,
+    T: Eq + Hash + Borrow<str> + ToString,
+{
+    let mut errors = vec![];
+    document.traverse(&arguments::ArgumentNameUniqueness(database), &mut errors);
+    document.traverse(&arguments::ArgumentsAreInputTypes(database), &mut errors);
+    document.traverse(
+        &directives::SelfReferentialDirectives(database),
+        &mut errors,
+    );
+    document.traverse(&enums::EnumValues(database), &mut errors);
+    document.traverse(&extensions::SameTypeExtensions(database), &mut errors);
+    document.traverse(&fields::FieldNameUniqueness(database), &mut errors);
+    document.traverse(&fields::FieldsAreOutputTypes(database), &mut errors);
+    document.traverse(&fields::HasFields(database), &mut errors);
+    document.traverse(&inputs::SelfReferentialInputs(database), &mut errors);
+    document.traverse(&interfaces::ImplementsInterface(database), &mut errors);
+    document.traverse(&names::ReservedNames(database), &mut errors);
+    document.traverse(&names::UniqueNames(database), &mut errors);
+    document.traverse(&types::NamedTypesExist(database), &mut errors);
+    document.traverse(&unions::UnionMemberTypes(database), &mut errors);
+    errors
+}
